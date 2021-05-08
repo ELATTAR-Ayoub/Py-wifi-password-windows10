2ffc0bb613d53c9ab59a834a6038e63f9b252e58
ELATTAR-Ayoub <82353773+ELATTAR-Ayoub@users.noreply.github.com>
2021-05-08 23:48:16 +0000

    Add files via upload

---
+++ Py_wifi_password.py
@@ -0,0 +1,54 @@
+#-----------------------------Besmi allah-----------------------------------------------
+
+#This import allows you ur code to use system commands (windows cmd)
+import subprocess
+
+#Impoprt the re module allows you to use regular expressions. (find a specific text in some output and do something with it).
+import re
+
+command_output = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output = True).stdout.decode()
+
+#We gonna use the re modules so ewe can find regular expressions (specific text in output).
+#We gonna search for thye wifi names which always listed after "All User Profile" in the output of the command line
+profile_names = (re.findall("All User Profile     : (.*)\r", command_output))
+
+#We gonna create an empty list to store all wifi names in it.
+#User names and password will be saved.
+wifi_list = list()
+
+#if we didn't find profile names--> that's mean we don't have connection.
+#we will continue the process with just wifi we have connection with, and try to get there info.
+if len(profile_names) != 0:
+    for name in profile_names:
+        #every wifi connection will need its dictionnary to store info in.
+        # so we will create to every wifi a dict.
+        wifi_profile = dict()
+        #we gonna now to run "netsh wlan show  profiles <wifi name>" in cmd to get info.
+        #if the security key is present, we could hopefuly get there password.
+        wifi_info = subprocess.run(["netsh", "wlan", "show", "profile", name], capture_output = True).stdout.decode()
+        # we gonna use regular expression to look for the absent cases so we ignore them.
+        if re.search("Security Key            : Absent", wifi_info):
+            continue
+        else:
+            #assign the ssid of the wifi to the dict
+            wifi_profile["ssid"] = name
+            #for the cases that are not absent, we need to run the code "key=clear" to get there password
+            profile_pass = subprocess.run(["netsh", "wlan", "show", "profile", name, "key=clear"], capture_output = True).stdout.decode()
+            #after that we gonna searsh for "Key Content" with re method to find the password.
+            password = re.search("Key Content            : (.*)\r", profile_pass)
+            #if we found a password in the regular expression, we gonna store it. otherwise we won't.
+            if password == None:
+                wifi_profile["password"] = None
+            else:
+                wifi_profile["password"] = password[1]
+            #we append(add) the wifi information to the wifi list
+            wifi_list.append(wifi_profile)
+
+#Finally we gonna print the passwords and info we got
+print("The list of the connected wi-fi connection:")
+for i in range(0, len(wifi_list)):
+    print(wifi_list[i])
+
+
+
+
